2ec3d5c717fb04603d5493c038b8fec7d699a964
Steven-Quihuire <squihuire@vimcoretechnologies.com>
2026-08-24 14:42:23 -0500

    fix(lint): restore root lint gate

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,12 @@
 import { baseConfig } from './packages/eslint-config/base';
 import { reactConfig } from './packages/eslint-config/react';
 
-export default [...baseConfig, ...reactConfig];
+export default [
+  {
+    // Root-level scratch/debug files are not part of any tsconfig project;
+    // type-aware rules cannot lint them.
+    ignores: ['mini-test.mts', 'repro-form.mts', 'sum.ts', 'sum.test.ts'],
+  },
+  ...baseConfig,
+  ...reactConfig,
+];
